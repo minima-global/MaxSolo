7d77d2efed78ae83f0bc4117dcfacba0d10b5452
Arnis Pridans <arnis@arnimedia.co.uk>
2022-12-12 23:25:45 +0000

    DD-1 Send token update
    Send token update

--- src/component/chat/ChatArea/ChatArea.js
+++ src/component/chat/ChatArea/ChatArea.js
@@ -336,9 +336,9 @@ const ChatArea = ({loadMessages, restartContacts, getBalance, getCurrentAddress,
                         Send image
                       </label>
                     </div>
-                    {/* <div>
+                    <div className='chat-area-footer-menu-button'>
                       <button onClick={onMenuClick} className="minima-btn btn-fill-black-medium">Cancel</button>
-                    </div> */}
+                    </div>
                 </div>
 
                 <div className="chat-area-footer">   
@@ -357,6 +357,7 @@ const ChatArea = ({loadMessages, restartContacts, getBalance, getCurrentAddress,
                     <svg className='form-send' onClick={submitHandle} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>
                   </form>
                 </div>
+
             </div>
 
           <ScrollBottom/>
--- src/elements/loader/LoaderSidebar.js
+++ src/elements/loader/LoaderSidebar.js
@@ -9,7 +9,7 @@ const LoaderSidebar = ({welcomeAddContact}) => {
                 <img className="welcome-contacts-body-icon" src={ChatIcon} alt="Add contact to Maxima" />
                 <div className='welcome-contacts-body-header'>Add a contact to Maxima<br /> to start chatting!</div>
                 <div className='welcome-contacts-body-copy'>Looks like you don’t have any contacts right now, add a contact or share your address with another user to get started.</div>
-                <button onClick={welcomeAddContact(1)} className="minima-btn btn-fill-blue-medium">Add contact</button>  
+                <button onClick={welcomeAddContact(2)} className="minima-btn btn-fill-blue-medium">Add contact</button>  
                 <button onClick={welcomeAddContact(4)} className="minima-btn btn-fill-black-medium">Share my address</button>    
             </div>
         </>
