6517a8d4467b7cd7a18415753c13ab167a7168e2
Elias <eliasnemr@gmail.com>
2023-06-13 16:51:37 +0300

    Change logs
    - fix error messages

--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,90 +1,111 @@
-import React, { useState, useEffect } from 'react';
-import ChatWindow from '../component/chat/ChatWindow';
-import WelcomeMaxSolo from '../component/welcome/WelcomeMaxSolo';
-import WelcomeLogo from '../component/welcome/WelcomeLogo';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Logoimage from '../assets/images/maxsolo_blue_logo.svg';
-
+import React, { useState, useEffect } from "react";
+import ChatWindow from "../component/chat/ChatWindow";
+import WelcomeMaxSolo from "../component/welcome/WelcomeMaxSolo";
+import WelcomeLogo from "../component/welcome/WelcomeLogo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Logoimage from "../assets/images/maxsolo_blue_logo.svg";
 
 const DashBoard = () => {
+  const [showWelcome, setShowWelcome] = useState(true);
+  const [howItWorks, setHowItWorks] = useState(false);
+  const [showLogo, setShowLogo] = useState(true);
+  const [isAppFirstLaunched, setIsAppFirstLaunched] = useState(null);
 
-    const [showWelcome, setShowWelcome] = useState(true);
-    const [howItWorks, setHowItWorks] = useState(false);
-    const [showLogo, setShowLogo] = useState(true);
-    const [isAppFirstLaunched, setIsAppFirstLaunched] = useState(null);
-
-    useEffect(() => {
-        async function fetchData() {
-            const appData = await AsyncStorage.getItem('isAppFirstLaunched');
-            if (appData == null) {
-              setIsAppFirstLaunched(true);
-              AsyncStorage.setItem('isAppFirstLaunched', 'false');
-            } else {
-              setIsAppFirstLaunched(false);
-            }
-        }
-        fetchData();
-        // AsyncStorage.removeItem('isAppFirstLaunched');
-    }, []);
-
-    const WelcomeHandler = () => {
-        setShowWelcome(false)
+  useEffect(() => {
+    async function fetchData() {
+      const appData = await AsyncStorage.getItem("isAppFirstLaunched");
+      if (appData == null) {
+        setIsAppFirstLaunched(true);
+        AsyncStorage.setItem("isAppFirstLaunched", "false");
+      } else {
+        setIsAppFirstLaunched(false);
+      }
     }
+    fetchData();
+    // AsyncStorage.removeItem('isAppFirstLaunched');
+  }, []);
 
-    const HowItWorks = () => {
-        setHowItWorks(!howItWorks);
-    }
+  const WelcomeHandler = () => {
+    setShowWelcome(false);
+  };
 
-    useEffect(()=>{
-        setTimeout(function() {
-            setShowLogo(false)
-        }, 2100);
-    },[])
-    
+  const HowItWorks = () => {
+    setHowItWorks(!howItWorks);
+  };
 
-    const WelcomeButton = () => {
-        return (
-            <>
-            <div className='section-heading welcome-button'>
-                <button data-testid="start-chat-button" onClick={WelcomeHandler} className="minima-btn btn-fill-blue-medium">Start chatting</button>
-                <button onClick={HowItWorks} className="minima-btn btn-fill-black-medium">How it works</button>
-            </div>
-            {howItWorks ?
-                <div className='how-it-works'>
-                    <div className='maxsolo-logotype'>
-                        <img className="light-version-logo" src={Logoimage} alt="logo" />
-                    </div>      
-                    <div className="section-heading heading-left">
-                        <h2 className="title">How it works</h2>
-                        <div>
-                        Experience freedom of information<br /><br />
-                        MaxSolo is an encrypted, peer-to-peer messaging application, which uses Minima’s information transfer layer, Maxima.<br /><br />
-                        <ul>
-                            <li>Send messages and images</li>
-                            <li>Transfer Minima, tokens and NFTs</li>
-                            <li>Experience freedom of information</li>
-                        </ul>
-                        </div>
-                        <div className='section-heading welcome-button'>
-                            <button onClick={WelcomeHandler} className="minima-btn btn-fill-blue-medium">Start chatting</button>
-                        </div>
-                    </div>
-                 </div>
+  useEffect(() => {
+    setTimeout(function () {
+      setShowLogo(false);
+    }, 2100);
+  }, []);
 
-				: null}
-            </>
-        )
-     }
-
+  const WelcomeButton = () => {
     return (
-        <>
-        <main className='app'>
-            { isAppFirstLaunched && showLogo ? <WelcomeLogo />  : null} 
-            { isAppFirstLaunched && showWelcome ? <WelcomeMaxSolo /> : <ChatWindow /> } 
-            { isAppFirstLaunched && showWelcome ? <WelcomeButton />: null} 
-        </main> 
-        </>
-    )
-}
+      <>
+        <div className="section-heading welcome-button">
+          <button
+            data-testid="start-chat-button"
+            onClick={WelcomeHandler}
+            className="minima-btn btn-fill-blue-medium"
+          >
+            Start chatting
+          </button>
+          <button
+            onClick={HowItWorks}
+            className="minima-btn btn-fill-black-medium"
+          >
+            How it works
+          </button>
+        </div>
+        {howItWorks ? (
+          <div className="how-it-works">
+            <div className="maxsolo-logotype">
+              <img className="light-version-logo" src={Logoimage} alt="logo" />
+            </div>
+            <div className="section-heading heading-left">
+              <h2 className="title">How it works</h2>
+              <div>
+                Experience freedom of information
+                <br />
+                <br />
+                MaxSolo is an encrypted, peer-to-peer messaging application,
+                which uses Minima’s information transfer layer, Maxima.
+                <br />
+                <br />
+                <ul>
+                  <li>Send messages and images</li>
+                  <li>Transfer Minima, tokens and NFTs</li>
+                  <li>Experience freedom of information</li>
+                </ul>
+              </div>
+              <div className="section-heading welcome-button">
+                <button
+                  onClick={WelcomeHandler}
+                  className="minima-btn btn-fill-blue-medium"
+                >
+                  Start chatting
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <main className="app">
+        {isAppFirstLaunched && showLogo ? <WelcomeLogo /> : null}
+        {isAppFirstLaunched && showWelcome ? (
+          <WelcomeMaxSolo />
+        ) : (
+          <ChatWindow />
+        )}
+        {isAppFirstLaunched && showWelcome ? <WelcomeButton /> : null}
+      </main>
+    </>
+  );
+};
 
 export default DashBoard;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,12 @@
 /**
-* MAXCHAT utility functions
-* 
-* @spartacusrex
-*/
+ * MAXCHAT utility functions
+ *
+ * @spartacusrex
+ */
 
 import { keyframes } from "@emotion/react";
-import MinimaSign from '../assets/images/minima-sign.svg'
-import {useEffect} from 'react';
+import MinimaSign from "../assets/images/minima-sign.svg";
+import { useEffect } from "react";
 
 const revealAnim = keyframes`
   from {
@@ -22,45 +22,49 @@ const revealAnim = keyframes`
 
 const utf8encoder = new TextEncoder();
 
-const hexToUtf8 = (s) =>
-{
+const hexToUtf8 = (s) => {
   return decodeURIComponent(
-     s.replace(/\s+/g, '') // remove spaces
-      .replace(/[0-9A-F]{2}/g, '%$&') // add '%' before each 2 characters
+    s
+      .replace(/\s+/g, "") // remove spaces
+      .replace(/[0-9A-F]{2}/g, "%$&") // add '%' before each 2 characters
   );
-}
+};
 
-const utf8ToHex = (s) =>
-{
+const utf8ToHex = (s) => {
   const rb = utf8encoder.encode(s);
-  let r = '';
+  let r = "";
   for (const b of rb) {
-    r += ('0' + b.toString(16)).slice(-2);
+    r += ("0" + b.toString(16)).slice(-2);
   }
   return r;
-}
+};
 
 const getTime = (date) => {
-  const rawDate = new Date(parseInt(date,10))
-  const time = rawDate.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  const rawDate = new Date(parseInt(date, 10));
+  const time = rawDate.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return time;
 };
 
 const tokenName = (item) => {
   if (item === undefined) {
-    return "Minima" 
-  }else{
+    return "Minima";
+  } else {
     return item;
   }
-}
+};
 
 const tokenUrl = (item) => {
   if (item === undefined) {
-    return MinimaSign; 
-  }else{
-    return item.replace('<artimage>','data:image/jpeg;base64,').replace('</artimage>','');
+    return MinimaSign;
+  } else {
+    return item
+      .replace("<artimage>", "data:image/jpeg;base64,")
+      .replace("</artimage>", "");
   }
-}
+};
 
 function ClickOutside(ref, onClickOutside) {
   useEffect(() => {
@@ -82,21 +86,25 @@ function ClickOutside(ref, onClickOutside) {
 }
 
 const openInNewTab = (url) => {
-  const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-  if (newWindow) newWindow.opener = null
-}
+  const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+  if (newWindow) newWindow.opener = null;
+};
 
-function showNotification(from, message){
-	//Only show if we can''t see it..
-	if(document.visibilityState !== "visible") {   
-		// If it's okay let's create a notification
-		if (Notification.permission === "granted") {
-	      const notification = new Notification(from,{
-			  body: message,
-			  icon: './minimalogo.png'
-			});
-	  }
-	}
+function showNotification(from, message) {
+  //Only show if we can''t see it..
+  if (document.visibilityState !== "visible") {
+    // If it's okay let's create a notification
+    if (
+      Notification &&
+      "permission" in Notification &&
+      Notification.permission === "granted"
+    ) {
+      const notification = new Notification(from, {
+        body: message,
+        icon: "./minimalogo.png",
+      });
+    }
+  }
 }
 
 const RE_URL = /\w+:\/\/\S+/g;
@@ -105,16 +113,16 @@ function linkOutput(str) {
   let match;
   const results = [];
   let lastIndex = 0;
-  while (match = RE_URL.exec(str)) {
+  while ((match = RE_URL.exec(str))) {
     const link = match[0];
     if (lastIndex !== match.index) {
       const text = str.substring(lastIndex, match.index);
-      results.push(
-        <span key={results.length}>{text}</span>,
-      );
+      results.push(<span key={results.length}>{text}</span>);
     }
     results.push(
-      <a key={results.length} href={link} target="_blank" rel="noreferrer">{link}</a>
+      <a key={results.length} href={link} target="_blank" rel="noreferrer">
+        {link}
+      </a>
     );
     lastIndex = match.index + link.length;
   }
@@ -122,12 +130,20 @@ function linkOutput(str) {
     return str;
   }
   if (lastIndex !== str.length) {
-    results.push(
-      <span key={results.length}>{str.substring(lastIndex)}</span>,
-    );
+    results.push(<span key={results.length}>{str.substring(lastIndex)}</span>);
   }
   return results;
 }
 
-
-export {revealAnim, tokenName, tokenUrl, openInNewTab, hexToUtf8, utf8ToHex, getTime, ClickOutside, linkOutput, showNotification};
+export {
+  revealAnim,
+  tokenName,
+  tokenUrl,
+  openInNewTab,
+  hexToUtf8,
+  utf8ToHex,
+  getTime,
+  ClickOutside,
+  linkOutput,
+  showNotification,
+};
